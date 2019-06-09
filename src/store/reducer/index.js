d822f72740949be58df763b60978f5f9cb9ad7bc
DevilMayDead <36238435+DevilMayDead@users.noreply.github.com>
2019-06-10 02:38:46 +0800

    Add files via upload

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,1 +1,17 @@
+import { authenicateUname } from "../../utils/cookie";
+const fuckme = authenicateUname("sessionId");
+const userState = { isLogin: 0, uname: fuckme };
+export default (state = userState, action) => {
+  switch (action.type) {
+    case "FENTCH_LOGIN":
+      return Object.assign({}, state, { isLogin: 1, uname: action.uname });
+    case "FENTCH_LOGIN_ERROR":
+      return Object.assign({}, state, { isLogin: 0, uname: "" });
 
+    case "FENTCH_OUT": {
+      return Object.assign({}, state, { isLogin: 0, uname: "" });
+    }
+    default:
+      return state;
+  }
+};
