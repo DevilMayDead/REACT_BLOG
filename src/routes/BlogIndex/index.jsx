78fdacb3689ba001a3ca438a28c1c7827e795d17
DevilMayDead <36238435+DevilMayDead@users.noreply.github.com>
2019-06-10 02:35:22 +0800

    Add files via upload

--- src/routes/BlogIndex/index.jsx
+++ src/routes/BlogIndex/index.jsx
@@ -1,1 +1,46 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { withRouter } from "react-router-dom";
+class BlogIndex extends Component {
+  handleClick = () => {
+    const { Logout } = this.props;
+    Logout(this.props.history);
+  };
 
+  componentWillMount() {
+    // const { uname } = this.props;
+    // if (uname === "") {
+    //   this.props.history.replace("/");
+    // }
+  }
+
+  render() {
+    const { uname } = this.props;
+    return (
+      <div>
+        <h1>{uname}</h1>
+        <button onClick={this.handleClick}>exit</button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    // uname: state.uname
+  };
+};
+
+const mapStateToDispatch = dispatch => {
+  return {
+    Logout: history => {
+      dispatch({ type: "FENTCH_OUT", history });
+    }
+  };
+};
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapStateToDispatch
+  )(BlogIndex)
+);
